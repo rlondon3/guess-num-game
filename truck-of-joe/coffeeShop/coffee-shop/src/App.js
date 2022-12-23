e230f1c4acb593ee0484f186c53770202ca6bc7f
Raphael Smith <codesmithr@outlook.com>
2022-12-23 09:11:01 -0800

    featured products carousel add. reusable cards and button add.

--- truck-of-joe/coffeeShop/coffee-shop/src/App.js
+++ truck-of-joe/coffeeShop/coffee-shop/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import FeaturedItems from './products/featuredProducts';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <FeaturedItems />
     </div>
   );
 }
